b39c1ff69ccaf33626a10419aeed71587f63a07f
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2025-09-08 10:00:07 +0300

    Add files via upload

---
+++ Test/Isik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv24Timur
+{
+    internal class Isik
+    {
+        public string eesnimi;
+        public string perenimi = "Tundmatu";
+        public int synniaasta=2000;
+
+        public Isik() { }
+
+        public Isik(string eesnimi, string perenimi)
+        {
+            this.eesnimi = eesnimi;
+            this.perenimi = perenimi;
+        }
+
+        public void Prindi_andmed()
+        {
+            Console.WriteLine($"Isiku andmed: {eesnimi} {perenimi}, Sündinud: {synniaasta}");
+        }
+    }
+}
---
+++ Test/Osa3_Funktsioonid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv24Timur
+{
+    internal class Osa3_Funktsioonid
+    {
+    }
+}
