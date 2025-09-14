ec8ae484ac7cd90a0f5790f9d562a8fb71c832bc
Timur-Basirov <153905042+Timur-Basirov@users.noreply.github.com>
2025-09-14 23:36:33 +0300

    Update Osa3_Funktsioonid.cs

--- Test/Osa3_Funktsioonid.cs
+++ Test/Osa3_Funktsioonid.cs
@@ -1,84 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace TARpv24Timur
+namespace ConsoleApp1
 {
-    public class Osa3_Funktsioonid
+    internal class Osa3_funktsioonid
     {
-        internal class Inimene
+        public static double[] Tekstist_arvud()
         {
-            public string eesnimi;
-            public int synniaasta = 2000;
+            Console.WriteLine("Sisesta arvud koma või tühikuga eraldatult: ");
+            string sisend = Console.ReadLine();
+            char[] eraldajad = new char[] { ' ' };//
 
-            public Inimene() { }
+            string[] osad = sisend.Split(eraldajad, StringSplitOptions.RemoveEmptyEntries);
 
-            public Inimene(string eesnimi)
+            double[] arvud = new double[osad.Length];
+            for (int i = 0; i < osad.Length; i++)
             {
-                this.eesnimi = eesnimi;
+                arvud[i] = Convert.ToDouble(osad[i]);
             }
-            //public static double[] Tekstist_arvud()
-            //{
-            //    Console.WriteLine("Sisesta arvud koma või tühikuga eraldatult: ");
-            //    string sisend = Console.ReadLine();
-            //    char[] eraldajad = new char[] { ' ' };
-
-            //    string[] osad = sisend.Split(eraldajad, StringSplitOptions.RemoveEmptyEntries);
-
-            //    double[] arvud = new double[osad.Length];
-            //    for (int i = 0; i < osad.Length; i++)
-            //    {
-            //        arvud[i] = Convert.ToDouble(osad[i]);
-            //    }
-            //    return arvud;
-            //}
-            //public static Tuple<double, double, double> AnalüüsiArve(double[] arvud)
-            //{
-            //    double summa = arvud.Sum();
-            //    double keskmine = arvud.Average();
-            //    double korrutis = 1;
-            //    foreach (double arv in arvud)
-            //    {
-            //        korrutis *= arv;
-            //    }
-
-            //    return Tuple.Create(summa, keskmine, korrutis);
-            //}
-            //public static int[] GenereeriRuudud(int min,int max)
-            //{
-            Random rnd = new Random();
-            //int n = rnd.Next(min, max);
-            //int m = rnd.Next(min, max);
-            //Console.WriteLine(m);
-            //Console.WriteLine(n);
-            //Console.WriteLine(Math.Min(n, m));
-            //int[] massive = new int[Math.Abs(m-n)];
-            //int k = 0;
-
-            //if (m<n)
-            //{
-            //    for (int i =m; i<=n ; i++)
-            //    {
-            //        massive[k] = i * 1;
-            //        k++;
-
-            //    }
-            //}
-            //else
-            //{
-            //    for (int i = n; i<=m ;i++)
-            //    {
-            //        massive[k] = i * 1;
-            //        k++;
-            //    }
-            //}
-            //return massive;
-
+            return arvud;
         }
+        public static Tuple<double, double, double> AnalüüsiArve(double[] arvud)
+        {
+            double summa = arvud.Sum();
+            double keskmine = arvud.Average();
+            double korrutis = 1;
+            foreach (double arv in arvud)
+            {
+                korrutis *= arv;
+            }
+            return Tuple.Create(summa, keskmine, korrutis);
+        }
+
     }
 }
-
